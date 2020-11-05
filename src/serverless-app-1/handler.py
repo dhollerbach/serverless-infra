678e9ebbbba3afc7f2945f0746aec19becb216da
dhollerbach <dave.hollerbach@gmail.com>
2020-11-05 16:38:34 -0700

    musing event bridge

--- src/serverless-app-1/handler.py
+++ src/serverless-app-1/handler.py
@@ -1,11 +1,19 @@
+import boto3
 import json
+import os
+
+#   Environment Variables
+EVENT_BRIDGE_SOURCE = os.environ['EVENT_BRIDGE_SOURCE']
+EVENT_BUS_NAME = os.environ['EVENT_BUS_NAME']
 
 
-def hello(event, context):
+def main(event, context):
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "message": "Successfully sent to EventBridge",
         "input": event
     }
+
+    put_event()
 
     response = {
         "statusCode": 200,
@@ -14,11 +22,25 @@ def hello(event, context):
 
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+
+def put_event():
+    client = boto3.client('events')
+    try:
+        detail = {
+            "key": "value"
+        }
+        response = client.put_events(
+            Entries=[
+                {
+                    'Source': EVENT_BRIDGE_SOURCE,
+                    'DetailType': 'test',
+                    'Detail': json.dumps(detail),
+                    'EventBusName': EVENT_BUS_NAME
+                }
+            ]
+        )
+        print(response)
+        print('Sent message to EventBridge')
+    except Exception as e:
+        print(e)
+        print('Failed to send message to EventBridge')
